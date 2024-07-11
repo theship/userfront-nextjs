a95ad5429c3d560bffad396829099d3d27198f49
@theship <juburdekin@gmail.com, julee@abevy.com>
2024-07-11 12:52:44 -0700

    added protected route with verify JWT and async getUser

--- app/(secure)/dashboard/page.tsx
+++ app/(secure)/dashboard/page.tsx
@@ -1,8 +1,11 @@
+// app/(secure)/dashboard/page.tsx
+
 "use client";
 
 import * as React from "react";
 import { LogoutButton, useUserfront } from "@userfront/next/client";
 import Link from "next/link";
+import UserDataComponent from './UserDataComponent';
 
 export default function DashboardPage() {
   const { user } = useUserfront();
@@ -10,8 +13,9 @@ export default function DashboardPage() {
   return (
     <div className="container flex flex-col p-4 space-y-4">
       <h1>
-      <span>Hello, {user.email}</span>
+        <span>Hello, {user.email}</span>
       </h1>
+      <UserDataComponent />
       <LogoutButton />
       <Link href="/server">Server Example with Async Data Fetching &#8594;</Link>
     </div>
